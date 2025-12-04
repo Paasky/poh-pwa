ca59da0efec857d8695474ad449695df454a3955
Paasky <pekko.tuomisto@gmail.com>
2025-12-04 19:35:40 +0100

    cleanup

--- src/components/PlayerDetails/Tabs/GovernmentTab.vue
+++ src/components/PlayerDetails/Tabs/GovernmentTab.vue
@@ -38,20 +38,26 @@ const rows = computed(() => {
       <tbody>
         <tr v-for="row in rows" :key="row.key">
           <td v-for="type in row.types" :key="type.key" style="height: 9rem">
-            <div class="d-flex ga-1">
-              <!-- todo children take up full width, same width for both -->
-              <UiObjectChip :type="type" size="lg" />
-
-              <!-- todo show text -->
-              <UiButton variant="tonal">Select</UiButton>
-            </div>
-            <div class="d-flex flex-column ga-1">
-              <UiYields :yields="type.yields" />
-            </div>
-            <div>
-              <!-- todo attach to bottom of card -->
-              <UiObjectChips :types="type.specials" />
-            </div>
+            <v-card
+              class="h-100 d-flex flex-column ga-2 my-2"
+              variant="outlined"
+              color="disabled"
+              style="border-radius: 0.5rem"
+            >
+              <!-- two children on the same row, together full width, equally wide (no extra elements, no custom css) -->
+              <div class="d-flex w-100 ga-2">
+                <UiObjectChip :type="type" color="secondary" size="lg" style="flex: 1 1 0" />
+                <UiButton variant="tonal" text="Select" style="flex: 1 1 0" />
+              </div>
+              <div class="d-flex flex-column ga-1">
+                <UiYields :yields="type.yields" />
+              </div>
+              <v-spacer />
+              <!-- todo: make children (chips is a v-for) go on a single row and fill the space -->
+              <div class="d-flex w-100 ga-1">
+                <UiObjectChips :types="type.specials" />
+              </div>
+            </v-card>
           </td>
         </tr>
       </tbody>
--- src/components/UiLegacy/UiCardGroup.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-  <div class="px-2 pb-2 pt-1 bg-green-950 grid gap-1 rounded-lg content-start h-full">
-    <slot />
-  </div>
-</template>
-
-<style scoped></style>
