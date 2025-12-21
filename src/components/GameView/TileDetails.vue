116c650aabce91add3f7cfd558a24f45a88e9166
Paasky <pekko.tuomisto@gmail.com>
2025-12-21 09:50:46 +0100

    selected tile in tile details

--- src/components/GameView/TileDetails.vue
+++ src/components/GameView/TileDetails.vue
@@ -3,56 +3,84 @@ import { useCurrentTile } from "@/stores/currentTile";
 import UiObjectChip from "@/components/Ui/UiObjectChip.vue";
 import { useObjectsStore } from "@/stores/objectStore";
 
-const { hoveredTile } = useCurrentTile();
+const { selectedTile } = useCurrentTile();
 </script>
 
 <template>
-  <v-sheet class="d-flex flex-column ga-1" color="secondary" style="width: 12rem; overflow: hidden">
-    <div
-      class="d-flex flex-column ga-1"
-      v-if="hoveredTile && useObjectsStore().currentPlayer.knownTileKeys.value.has(hoveredTile.key)"
+  <div
+    v-if="selectedTile && useObjectsStore().currentPlayer.knownTileKeys.value.has(selectedTile.key)"
+    class="d-flex ga-1"
+  >
+    <v-sheet
+      class="d-flex flex-column ga-1 pa-1"
+      color="secondary"
+      style="width: 12rem; overflow: hidden"
     >
-      <div class="d-flex ga-1">
-        <UiObjectChip :type="hoveredTile.elevation" />
-        <UiObjectChip :type="hoveredTile.terrain" />
+      <div class="d-flex flex-column ga-1">
+        <div class="d-flex ga-1">
+          <UiObjectChip :type="selectedTile.elevation" />
+          <UiObjectChip :type="selectedTile.terrain" />
+        </div>
+        <div
+          class="d-flex ga-1"
+          v-if="selectedTile.feature.value || selectedTile.isFresh || selectedTile.isSalt"
+        >
+          <UiObjectChip v-if="selectedTile.feature.value" :type="selectedTile.feature.value" />
+          <UiObjectChip v-if="selectedTile.isFresh" type="conceptType:freshWater" />
+          <span v-if="selectedTile.isSalt">(Salt Water)</span>
+        </div>
+        <div
+          class="d-flex ga-1"
+          v-if="
+            selectedTile.resource.value ||
+            selectedTile.naturalWonder ||
+            selectedTile.pollution.value
+          "
+        >
+          <UiObjectChip
+            v-if="selectedTile.resource.value"
+            :type="selectedTile.resource.value.name"
+          />
+          <UiObjectChip v-if="selectedTile.naturalWonder" :type="selectedTile.naturalWonder.name" />
+          <UiObjectChip
+            v-if="selectedTile.pollution.value"
+            :type="selectedTile.pollution.value.name"
+          />
+        </div>
+        <div v-if="selectedTile.playerKey.value">
+          Owner: ({{ selectedTile.player.value!.name }})
+        </div>
+        <div class="d-flex ga-1">
+          <UiObjectChip :type="selectedTile.climate" />
+          <UiObjectChip :type="selectedTile.domain" />
+        </div>
+        <div class="d-flex ga-1">
+          <span v-if="selectedTile.riverKey">({{ selectedTile.river.value!.name }})</span>
+          <UiObjectChip :type="selectedTile.area" />
+        </div>
       </div>
-      <div
-        class="d-flex ga-1"
-        v-if="hoveredTile.feature.value || hoveredTile.isFresh || hoveredTile.isSalt"
-      >
-        <UiObjectChip v-if="hoveredTile.feature.value" :type="hoveredTile.feature.value" />
-        <UiObjectChip v-if="hoveredTile.isFresh" type="conceptType:freshWater" />
-        <span v-if="hoveredTile.isSalt">(Salt Water)</span>
+    </v-sheet>
+
+    <v-sheet
+      v-if="selectedTile.unitKeys.value.length > 0 || selectedTile.constructionKey.value"
+      class="d-flex flex-column ga-1 pa-1"
+      color="secondary"
+      style="width: 12rem; overflow: hidden"
+    >
+      <div v-if="selectedTile.construction.value" class="d-flex flex-column ga-1">
+        <div class="text-caption">Construction:</div>
+        <UiObjectChip :type="selectedTile.construction.value.type" />
       </div>
-      <div
-        class="d-flex ga-1"
-        v-if="
-          hoveredTile.resource.value || hoveredTile.naturalWonder || hoveredTile.pollution.value
-        "
-      >
-        <UiObjectChip v-if="hoveredTile.resource.value" :type="hoveredTile.resource.value.name" />
-        <UiObjectChip v-if="hoveredTile.naturalWonder" :type="hoveredTile.naturalWonder.name" />
-        <UiObjectChip v-if="hoveredTile.pollution.value" :type="hoveredTile.pollution.value.name" />
+      <div v-if="selectedTile.unitKeys.value.length > 0" class="d-flex flex-column ga-1">
+        <div class="text-caption">Units:</div>
+        <div class="d-flex flex-wrap ga-1">
+          <span v-for="unit of selectedTile.units.value" :key="unit.key"
+            >({{ unit.design.value.name }})</span
+          >
+        </div>
       </div>
-      <div v-if="hoveredTile.playerKey.value">Owner: ({{ hoveredTile.player.value!.name }})</div>
-      <div v-if="hoveredTile.unitKeys.value.length > 0" class="d-flex ga-1">
-        Units:
-        <span v-for="unit of hoveredTile.units.value" :key="unit.key"> ({{ unit.name }}) </span>
-      </div>
-      <div class="d-flex ga-1">
-        <UiObjectChip :type="hoveredTile.climate" />
-        <UiObjectChip :type="hoveredTile.domain" />
-      </div>
-      <div class="d-flex ga-1">
-        <span v-if="hoveredTile.riverKey">({{ hoveredTile.river.value!.name }})</span>
-        <UiObjectChip :type="hoveredTile.area" />
-      </div>
-    </div>
-    <div v-else-if="hoveredTile">(Terra Incognita)</div>
-    <v-code v-if="hoveredTile" class="mt-auto">
-      {{ hoveredTile.key }}
-    </v-code>
-  </v-sheet>
+    </v-sheet>
+  </div>
 </template>
 
 <style scoped></style>
